b74863cd6e3b30e14248a8e8a9dfbc2345168828
Subham <sarkarsubham087@gmail.com>
2026-06-05 21:51:05 +0530

    some changes

--- app/main.py
+++ app/main.py
@@ -26,4 +26,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my api, my name is @"} 
+    return {"message": "Welcome to my api, this is a social media app"} 
